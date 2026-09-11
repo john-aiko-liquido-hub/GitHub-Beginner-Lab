8ca72753bcb69b0993ffc15cb7a3ec4bfa9572bd
John Aiko Liquido <User@Liquido>
2026-09-11 23:47:12 +0800

    Update student profile output

--- StudentProfile/StudentProfile/StudentProfile.cs
+++ StudentProfile/StudentProfile/StudentProfile.cs
@@ -19,7 +19,7 @@ namespace StudentProfile
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Student Profile: John Aiko Liquido");
+            MessageBox.Show("Student Profile — GitHub Beginner Lab");
         }
     }
 }
